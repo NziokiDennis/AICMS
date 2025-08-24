7a197cbaf9eac4482d1617ba0ed63fec510beca0
NziokiDennis <nziokikisilu@gmail.com>
2025-08-24 20:13:39 +0300

    project initialization

---
+++ landingpage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Landing Page</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <body>
+        <div class="container">
+            <h1>Welcome to Our Website</h1>
+            <p>Your journey starts here. Explore our features and offerings.</p>
+            <a href="signup.php" class="btn">Get Started</a>
+        </div>
+    </body>
+</head>
